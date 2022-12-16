762017e51db22a7c1d0466915506b14e581811e2
OSHI RAGHAV <95966191+xoshiraghavx@users.noreply.github.com>
2022-12-16 17:12:58 +0530

    uploaded files

---
+++ my-app/src/components/MainPage/MainPage.jsx
@@ -0,0 +1,16 @@
+import "./MainPage.css";
+import FilterSection from "./FilterSection";
+import QuestionBank from "./QuestionBank";
+
+function MainPage() {
+  return (
+    <div>
+      <div className="main-section">
+        <FilterSection />
+        <QuestionBank />
+      </div>
+    </div>
+  );
+}
+
+export default MainPage;
---
+++ my-app/src/components/MainPage/QuestionBank.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function QuestionBank() {
+  return (
+    <div>
+        <div className="ques-section">
+          <div className="ques-bar">
+            <p>#</p>
+            <p>Name</p>
+            <p>Level</p>
+            <p>Category</p>
+            <p>Solution</p>
+          </div>
+          <div className="ques-bank"> 
+          <div className="ques">
+            <p>1</p>
+            <p>osjhi</p>
+            <p>hhh</p>
+            <p>iii</p>
+            <p>ooo</p>
+          </div>
+          <hr />
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default QuestionBank;
